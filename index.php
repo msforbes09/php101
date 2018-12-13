9c0fd18b7ec24251aef39a357b41de64d5240bb9
Arnel Forbes <forbesarnel09@gmail.com>
2018-12-13 12:24:51 +0800

    study classes

--- index.php
+++ index.php
@@ -5,7 +5,33 @@ $info = [
 	'Age' => 27,
 	'Hair' => 'Black'
 ];
-var_dump($_SERVER);
+
+class Tasks{
+	protected $description;
+
+	protected $completed = false;
+
+	public function __construct($description)
+	{
+		$this->description = $description;
+	}
+	public function finishTask()
+	{
+		$this->completed = true;
+	}
+
+}
+
+$newTask = [
+	new Tasks('practice php'),
+	new Tasks('study'),
+	new Tasks('sleep'),
+	new Tasks('clear data'),
+];
+
+$newTask[3]->finishTask();
+
+var_dump($newTask);
 
 die();
 
